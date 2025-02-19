d30fa6eb683c7fd36feb247af825d0e741aea697
rodzlen <qkqhrkxdkdy@naver.com>
2025-02-19 15:13:28 +0900

    fix db

--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 import os
-
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_PUuf6pzcO8Wr@ep-curly-morning-a8vitdnh-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://neondb_owner:npg_PUuf6pzcO8Wr@ep-curly-morning-a8vitdnh-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
 
 engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
